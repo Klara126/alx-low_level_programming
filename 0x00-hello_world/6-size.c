d683b345b0204b3881b2919c177a8184630293b1
kklara <klara@me.com>
2023-04-20 17:58:35 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entery
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of char: %i byte(s)\n", sizeof(char));
+	printf("size of int: %i byte(s)\n", sizeof(int));
+	printf("size of long int: %i byte(s)\n", sizeof(long int));
+	printf("size of float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
